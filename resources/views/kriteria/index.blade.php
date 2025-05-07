0375b2e5e1b07a5a68d14d2ccaf52f5a80b7481e
314dhan <ramadhan25321@gmail.com>
2025-05-07 10:51:34 +0700

    update new style for kriteria.index

--- resources/views/kriteria/index.blade.php
+++ resources/views/kriteria/index.blade.php
@@ -12,10 +12,6 @@
         Tambah Kriteria
     </button>
 
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-
     <!-- Modal Tambah -->
     <div class="modal fade" id="modalKriteriaTambah" tabindex="-1" aria-labelledby="modalLabelTambah" aria-hidden="true">
     <div class="modal-dialog">
@@ -61,88 +57,97 @@
     </div>
     </div>
 
-
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Kode</th>
-                <th>Nama</th>
-                <th>Bobot</th>
-                <th>Jenis</th>
-                <th>Deskripsi</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($kriteria as $item)
+    <div class="row">
+        <div class="col-md-3">
+            @include('layouts.sidebar')
+        </div>
+        <div class="col-md-9">
+            @if(session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            <table class="table table-bordered">
+            <thead>
                 <tr>
-                    <td>{{ $item->kode_kriteria }}</td>
-                    <td>{{ $item->nama_kriteria }}</td>
-                    <td>{{ $item->bobot }}</td>
-                    <td>{{ ucfirst($item->jenis) }}</td>
-                    <td>{{ $item->description }}</td>
-                    <td>
-                        <!-- Tombol Edit -->
-                        <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdit{{ $item->id }}">
-                        Edit
-                        </button>
-
-                        <!-- Modal Edit -->
-                        <div class="modal fade" id="modalEdit{{ $item->id }}" tabindex="-1">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <form action="{{ route('kriteria.update', $item->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="modal-header">
-                                <h5 class="modal-title">Edit Kriteria</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                </div>
-                                <div class="modal-body">
-                                <div class="mb-3">
-                                    <label>Kode Kriteria</label>
-                                    <input type="text" name="kode_kriteria" value="{{ $item->kode_kriteria }}" class="form-control">
-                                </div>
-                                <div class="mb-3">
-                                    <label>Nama Kriteria</label>
-                                    <input type="text" name="nama_kriteria" value="{{ $item->nama_kriteria }}" class="form-control">
-                                </div>
-                                <div class="mb-3">
-                                    <label>Bobot</label>
-                                    <input type="number" name="bobot" step="0.01" max="1" value="{{ $item->bobot }}" class="form-control">
-                                </div>
-                                <div class="mb-3">
-                                    <label>Jenis</label>
-                                    <select name="jenis" class="form-select">
-                                    <option value="cost" {{ $item->jenis == 'cost' ? 'selected' : '' }}>Cost</option>
-                                    <option value="benefit" {{ $item->jenis == 'benefit' ? 'selected' : '' }}>Benefit</option>
-                                    </select>
-                                </div>
-                                <div class="mb-3">
-                                    <label>Deskripsi</label>
-                                    <textarea name="description" class="form-control">{{ $item->description }}</textarea>
-                                </div>
-                                </div>
-                                <div class="modal-footer">
-                                <button type="submit" class="btn btn-warning">Update</button>
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                </div>
-                            </form>
-                            </div>
-                        </div>
-                        </div>
-
-                        <!-- Hapus -->
-                        <form action="{{ route('kriteria.destroy', $item->id) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button onclick="return confirm('Yakin hapus?')" class="btn btn-sm btn-danger">Hapus</button>
-                        </form>
-                    </td>
+                    <th>Kode</th>
+                    <th>Nama</th>
+                    <th>Bobot</th>
+                    <th>Jenis</th>
+                    <th>Deskripsi</th>
+                    <th>Aksi</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($kriteria as $item)
+                    <tr>
+                        <td>{{ $item->kode_kriteria }}</td>
+                        <td>{{ $item->nama_kriteria }}</td>
+                        <td>{{ $item->bobot }}</td>
+                        <td>{{ ucfirst($item->jenis) }}</td>
+                        <td>{{ $item->description }}</td>
+                        <td>
+                            <!-- Tombol Edit -->
+                            <button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdit{{ $item->id }}">
+                            Edit
+                            </button>
+
+                            <!-- Modal Edit -->
+                            <div class="modal fade" id="modalEdit{{ $item->id }}" tabindex="-1">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <form action="{{ route('kriteria.update', $item->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="modal-header">
+                                    <h5 class="modal-title">Edit Kriteria</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label>Kode Kriteria</label>
+                                        <input type="text" name="kode_kriteria" value="{{ $item->kode_kriteria }}" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label>Nama Kriteria</label>
+                                        <input type="text" name="nama_kriteria" value="{{ $item->nama_kriteria }}" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label>Bobot</label>
+                                        <input type="number" name="bobot" step="0.01" max="1" value="{{ $item->bobot }}" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label>Jenis</label>
+                                        <select name="jenis" class="form-select">
+                                        <option value="cost" {{ $item->jenis == 'cost' ? 'selected' : '' }}>Cost</option>
+                                        <option value="benefit" {{ $item->jenis == 'benefit' ? 'selected' : '' }}>Benefit</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label>Deskripsi</label>
+                                        <textarea name="description" class="form-control">{{ $item->description }}</textarea>
+                                    </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="submit" class="btn btn-warning">Update</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                    </div>
+                                </form>
+                                </div>
+                            </div>
+                            </div>
+
+                            <!-- Hapus -->
+                            <form action="{{ route('kriteria.destroy', $item->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button onclick="return confirm('Yakin hapus?')" class="btn btn-sm btn-danger">Hapus</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
 
